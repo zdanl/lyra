de54734edcca58bb756b7ea03ebc5786e6c5a437
z <z@zulla.dev>
2023-11-30 10:24:38 +0000

    fixed hydration

--- serverless/appwrite_crypto/functions/hydrate_crypto_wallets/src/main.js
+++ serverless/appwrite_crypto/functions/hydrate_crypto_wallets/src/main.js
@@ -1,8 +1,8 @@
 import { ID, Client, Databases } from 'node-appwrite';
 import Web3 from 'web3';
 
-const eth_api_url = "https://mainnet.infura.io/v3/";
-const btc_api_url = `https://blockchain.info/address/`;
+const eth_api_url = "https://mainnet.infura.io/v3";
+const btc_api_url = `https://blockchain.info/address`;
 
 
 // This is the hydrate_crypto_wallets Appwrite function
@@ -20,11 +20,9 @@ const retrieve_eth_balance = async (wallet_address, infura_api_key) => {
     var web3Provider = new Web3.providers.HttpProvider(`${eth_api_url}/${infura_api_key}`);
     var web3 = new Web3(web3Provider);
 
-		web3.eth.getBalance(wallet_address, (err, balance) => {
-			if (err === null)
-				return web3.utils.fromWei(balance, 'ether');
-			else throw {success: false, error: {message: "Unable to fetch ETH Balance", stack: err}}
-		});
+		var balance = await web3.eth.getBalance(wallet_address);
+    balance = web3.utils.fromWei(balance, 'ether');
+    return balance;
 	} catch (ex) {
 		console.log(ex);
 		throw ex;
@@ -51,7 +49,7 @@ const retrieve_btc_balance = async (wallet_address) => {
 
 
 export default async ({ req, res, log, error }) => {
-  // Invoking the Appwrite SDK?
+// Invoking the Appwrite SDK?
   const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject('lyra')
@@ -64,24 +62,21 @@ export default async ({ req, res, log, error }) => {
   const resp = await databases.listDocuments('lyra', 'wallets');
   
   // do not by mistake transfer a new balance to a wrong wallet after exception
-  let eth_balance = 0;
-  let btc_balance = 0;
-  let hydration_count = 0;
+  var eth_balance = 0;
+  var btc_balance = 0;
+  var hydration_count = 0;
 
-  log(`Hydrating ${resp.total} wallets ...`);
+  console.log(`Hydrating ${resp.total} wallets ...`);
 
   for (var doc in resp.documents) {
     doc = resp.documents[doc];
-    console.log(`Doc: ${doc}`);
     eth_balance = 0;
     btc_balance = 0;
     console.log(`Retrieving ETH balance for ${doc.eth_address}`);
     eth_balance = await retrieve_eth_balance(doc.eth_address, infura_api_key);
-    console.log(`Amount: ${eth_balance}`);
     
     console.log(`Retrieving BTC balance for ${doc.btc_address}`);
     btc_balance = await retrieve_btc_balance(doc.btc_address);
-    console.log(`Amount: ${btc_balance}`);
 
     if (eth_balance !== doc.eth_balance || btc_balance !== doc.btc_balance) {
       hydration_count++;
@@ -97,10 +92,11 @@ export default async ({ req, res, log, error }) => {
 
   }
   
-  log(`All done. Hydrated ${hydration_count} wallets.`);
+  console.log(`All done. Hydrated ${hydration_count} wallets.`);
 
   /************ Write to DB **********/
 
   // `res.json()` is a handy helper for sending JSON
   return res.json({'status': true});
 };
+
